56539f0b573571ce56ec163808a5f24215594f23
clindsay107 <clindsay107@gmail.com>
2014-05-07 07:19:13 +0000

    refactored torrent class for better encapsulation and updating params

--- lib/pirata/api.rb
+++ lib/pirata/api.rb
@@ -65,7 +65,7 @@ module Pirata
           h[:title]       = row.search('.detLink')[0].text
           h[:url]         = @base_url + row.search('.detLink').attribute('href').to_s
           h[:id]          = h[:url].split('/')[4]
-          h[:magnet_link] = row.search('td a')[3]['href']
+          h[:magnet] = row.search('td a')[3]['href']
           h[:seeders]     = row.search('td')[2].text.to_i
           h[:leechers]    = row.search('td')[3].text.to_i
           h[:uploader]    = Pirata::User.new(row.search('td a')[5].text, @base_url)
@@ -81,13 +81,6 @@ module Pirata
   end
 end
 
-api = Pirata::API.new('http://thepiratebay.si')
-#Pirata::Torrent.find_by_id(10088431)
-#api.top
-collection = api.search('skyrim')
-#p collection.length
-#p collection.first
-#puts "-----"
-#collection = api.search('world of warcraft')
-#p collection.length
-#p collection.first
+#api = Pirata::API.new('http://thepiratebay.si')
+#collection = api.search('skyrim')
+#p collection.first.files
--- lib/pirata/torrent.rb
+++ lib/pirata/torrent.rb
@@ -4,21 +4,98 @@ require 'time'
 
 module Pirata
   class Torrent
-    
-    # Iniitialize a Torrent object with instance variables and getter methods
-    # from a supplied params hash
-    def initialize(params)
-      params.each do |k,v| 
-        instance_variable_set("@#{k}", v)
-        self.class.send(:attr_reader, k)
-      end
+
+    def initialize(params_hash)
+      @params = params_hash
     end
-      
+    
+    def title
+      @params[:title]
+    end
+    
+    def category
+      @params[:category]
+    end
+    
+    def title
+      @params[:title]
+    end
+    
+    def url
+      @params[:url]
+    end
+    
+    def id
+      @params[:id]
+    end
+    
+    def magnet
+      @params[:magnet]
+    end
+    
+    def seeders
+      @params[:seeders]
+    end
+    
+    def leechers
+      @params[:leechers]
+    end
+    
+    def uploader
+      @params[:uploader]
+    end
+    
+    def paginated?
+      @params[:paginated]
+    end
+    
+    def files
+      unless @params[:files]
+        update_params
+      end
+      @params[:files]
+    end
+    
+    def size
+      unless @params[:size]
+        update_params
+      end
+      @params[:size]
+    end
+    
+    def date
+      unless @params[:date]
+        update_params
+      end
+      @params[:date]
+    end
+    
+    def comments
+      unless @params[:comments]
+        update_params
+      end
+      @params[:comments]
+    end
+    
+    def hash
+      unless @params[:hash]
+        update_params
+      end
+      @params[:hash]
+    end
+           
     # Return a Torrent object from a corresponding ID
     def self.find_by_id(id)
       raise "Invalid torrent ID format. Must be an integer" if id.class != Fixnum
       html = Nokogiri::HTML(open(Pirata::Config::BASE_URL + "/torrent" + "/#{URI.escape(id.to_s)}"))
-      parse_torrent_page(html)
+      results_hash = parse_torrent_page(html)
+      Pirata::Torrent.new(results_hash)
+    end
+    
+    def update_params
+      html = Nokogiri::HTML(open(@params[:url]))
+      updated_params = Pirata::Torrent.parse_torrent_page(html)
+      @params.merge!(updated_params)
     end
     
     private #-------------------------------------------------
@@ -39,8 +116,9 @@ module Pirata
           :comments  => row.search('dd')[7].text.to_i,
           :hash      => row.search('dl').text.split.last.strip
         }
+        return h
       end
-      Torrent.new(h)
+      nil
     end
         
   end

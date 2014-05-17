5e86da80d1e72cba1f9821da4beb29ae61221bec
aormond <aormond@users.noreply.github.com>
2014-05-17 19:41:01 -0400

    Added some useful categories

--- lib/pirata/category.rb
+++ lib/pirata/category.rb
@@ -2,9 +2,13 @@ module Pirata
   module Category
     AUDIO         = "100"
     VIDEO         = "200"
+    MOVIES        = "201"
+    TV_SHOWS      = "205"
     APPLICATIONS  = "300"
     GAMES         = "400"
     PORN          = "500"
     OTHER         = "600"
+    EBOOKS        = "601"
+    COMICS        = "602"
   end
 end

c71641f282da2505ba47c3e83f523820f24d54e6
clindsay107 <clindsay107@gmail.com>
2016-01-26 13:01:18 -0800

    Fixes and closes #8 - update base URL and list of proxy sites

--- lib/pirata/config.rb
+++ lib/pirata/config.rb
@@ -4,13 +4,13 @@ module Pirata
     # This is the base URL to query against. Depending on server/mirror
     # availability, blocked URLs, etc, you may need to change this. A
     # list of available mirrors for ThePirateBay may be found at
-    # http://http://proxybay.info/
+    # https://proxybay.la/
     #
     # Note that all URLs should yeild the same results. You are advised
     # to pick a mirror that is closest to your application server for best
     # results though.
 
-    BASE_URL = "http://thepiratebay.mn"
+    BASE_URL = "https://ahoy.re"
 
     # This is the rule used when following HTTPS <-> HTTP redirects.
     # It accepts :all and :safe

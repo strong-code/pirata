d82d7d10da5b697e2e03af001be86c59a86ab7e8
clindsay107 <clindsay107@gmail.com>
2016-03-17 00:55:23 -0700

    Allow configuration through YAML or hash. TODO: test and show example YAML config

--- lib/pirata.rb
+++ lib/pirata.rb
@@ -1,4 +1,43 @@
 require 'pirata/search'
 require 'pirata/torrent'
 require 'pirata/category'
-require 'pirata/config'
+require 'yaml'
+
+module Pirata
+  @config = {
+    :base_url => 'https://pirateproxy.tv/',
+    :redirect => :all
+  }
+
+  def self.valid_key?(key)
+    @config.keys.include?(key.to_sym)
+  end
+
+  def self.configure(config)
+    if config.class == Hash
+      self.configure_from_hash(config)
+      return
+    elsif
+      File.exist?(config)
+      self.configure_from_file(config)
+      return
+    else
+      raise 'Must pass configuration as a hash or path to YAML file.'
+    end
+  end
+
+  def self.configure_from_hash(opts = {})
+    opts.each do |k, v|
+      @config[k.to_sym] = v if self.valid_key?(k)
+    end
+  end
+
+  def self.configure_from_file(path)
+    config = YAML::load(IO.read(path))
+  end
+
+  def self.config
+    @config
+  end
+
+end
--- pirata.gemspec
+++ pirata.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = "pirata"
-  s.version = "1.3.2"
-  s.date = "2016-01-26"
+  s.version = "2.0.0"
+  s.date = "2016-03-17"
   s.summary = "Pirata - a Ruby API for The Pirate Bay"
   s.authors = ["Colin Lindsay"]
   s.email = "clindsay107@gmail.com"
   s.homepage = "https://github.com/strong-code/pirata"
-  s.files = ["lib/pirata.rb", "lib/pirata/search.rb", "lib/pirata/category.rb", "lib/pirata/sort.rb", "lib/pirata/config.rb", "lib/pirata/torrent.rb", "lib/pirata/user.rb"]
+  s.files = ["lib/pirata.rb", "lib/pirata/search.rb", "lib/pirata/category.rb", "lib/pirata/sort.rb", "lib/pirata/torrent.rb", "lib/pirata/user.rb"]
   s.require_paths = ["lib", "lib/pirata"]
   s.license = "GPL-3.0"
   s.description = "A Ruby gem that exposes an API for using The Pirate Bay torrent tracker service."
